9e2df2477d396754e37c7635983844b42d3c716b
chowyb <chowwars@hotmail.com>
2014-05-24 14:12:43 +0800

    initial commit

---
+++ js/draw.js
@@ -0,0 +1,48 @@
+/** draw.js **/
+
+ 
+window.addEventListener("DOMContentLoaded", init, false); 
+function init() {
+    loadAndDrawImage("http://www.comp.nus.edu.sg/images/mySoC.png", 0, 0);
+    var canvas = document.getElementById("canvas");
+    canvas.addEventListener("mousedown", getPosition, false);
+}
+
+function loadAndDrawImage(url, x, y)
+{
+    // Create an image object. This is not attached to the DOM and is not part of the page.
+    var image = new Image();
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+ 
+    // When the image has loaded, draw it to the canvas
+    image.onload = function() {
+        context.drawImage(image, x, y);
+    };
+ 
+    // Now set the source of the image that we want to load
+    image.src = url;
+}
+
+function getPosition(event) {
+    var x = new Number();
+    var y = new Number();
+    var canvas = document.getElementById("canvas");
+
+    if (event.x != undefined && event.y != undefined) {
+        x = event.x;
+        y = event.y;
+    }
+    else { // Firefox method to get the position
+        x = event.clientX + document.body.scrollLeft +
+            document.documentElement.scrollLeft;
+        y = event.clientY + document.body.scrollTop +
+            document.documentElement.scrollTop;
+    }
+
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+	
+	
+	loadAndDrawImage("http://www.comp.nus.edu.sg/images/socLink.png", x, y);
+}
