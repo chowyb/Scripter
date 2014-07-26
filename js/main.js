ec80404acd71f9fd6d657e8c04e865a049254231
Yuan Bin <chowwars@hotmail.com>
2014-07-26 21:05:53 +0800

    corrected colours

--- js/main.js
+++ js/main.js
@@ -792,10 +792,10 @@ function Fruit(row, col, digestCost) {
 	
 	this.drawCurrPos = function() {
 		if (digestCost == 0) {
-			loadAndDrawImage("images/cpoint0.png", (this.col * 50) + 10, (this.row * 50) + 10);
+			loadAndDrawImage("images/cpoint1.png", (this.col * 50) + 10, (this.row * 50) + 10);
 		}
 		else if (digestCost > 5) {
-			loadAndDrawImage("images/cpoint1.png", (this.col * 50) + 10, (this.row * 50) + 10);
+			loadAndDrawImage("images/cpoint0.png", (this.col * 50) + 10, (this.row * 50) + 10);
 		}
 		else {
 			loadAndDrawImage(this.imagePath, (this.col * 50) + 10, (this.row * 50) + 10);
